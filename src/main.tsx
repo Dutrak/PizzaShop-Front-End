dbc7d7e8d0c85e67d90ecebebd515170c2b7f624
Dutrak <fernandodutra100701@gmail.com>
2024-08-12 15:49:28 -0300

    feat:setup msw

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { enableMSW } from './api/mocks/index.ts'
 import { App } from './app.tsx'
+
+await enableMSW()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
